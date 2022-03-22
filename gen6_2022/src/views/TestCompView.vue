7eb3fc77550e923a1a3503d2c114a12d0fcfcbdb
KanashiXI <panyapat.test@gmail.com>
2022-03-22 13:15:34 +0700

    train vue

---
+++ gen6_2022/src/views/TestCompView.vue
@@ -0,0 +1,15 @@
+<template>
+  <TestComp />
+</template>
+
+<script>
+import TestComp from '../components/TestComp'
+
+export default {
+  name: 'TestComp',
+
+  components: {
+    TestComp
+  }
+}
+</script>
